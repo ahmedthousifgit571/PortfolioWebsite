b427bbea577a746a7fa5f39465c4596c9e83a72e
ahmed thousif <ahmedthousif571@gmail.com>
2025-06-12 13:03:51 +0530

    counter

--- src/assets/constants/index.js
+++ src/assets/constants/index.js
@@ -34,10 +34,10 @@ const words = [
 ];
 
 const counterItems = [
-  { value: 15, suffix: "+", label: "Years of Experience" },
-  { value: 200, suffix: "+", label: "Satisfied Clients" },
-  { value: 108, suffix: "+", label: "Completed Projects" },
-  { value: 90, suffix: "%", label: "Client Retention Rate" },
+  { value: 1, suffix: "+", label: "Years of Experience" },
+  { value: 2, suffix: "+", label: "Satisfied Clients" },
+  { value: 7, suffix: "+", label: "Completed Projects" },
+  { value: 14, suffix: "+", label: "Tech Tools Mastered" },
 ];
 
 const logoIconsList = [
