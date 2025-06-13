84a53631afe596ea77935e1444344f9eca5a93ed
ahmed thousif <ahmedthousif571@gmail.com>
2025-06-13 20:01:16 +0530

    featuredCards C

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
+import LogoSection from './components/LogoSection'
 import NavBar from './components/NavBar'
 import './index.css'
+import FeatureCards from './sections/FeatureCards'
 import Hero from './sections/Hero'
 import ShowCaseSection from './sections/ShowCaseSection'
 
@@ -10,6 +12,8 @@ function App() {
     <NavBar />
     <Hero />
     <ShowCaseSection />
+    <LogoSection />
+    <FeatureCards />
    </div>
   )
 }
